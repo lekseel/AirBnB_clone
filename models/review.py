2a42b5b1c1e75833133474ea70dbbe65c0cd918b
Olalekan Shogbaike <111006863+lekseel@users.noreply.github.com>
2023-02-12 22:18:57 +0100

    good mind

--- models/review.py
+++ models/review.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Defines review class
-"""
+"""Contains the Review model"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Reviews made by users about a place"""
+    """Implements the Review model"""
     place_id = ""
     user_id = ""
     text = ""
