f47288d26155f991006eb2a6689b05c0bbc9dab3
Olalekan Shogbaike <111006863+lekseel@users.noreply.github.com>
2023-02-12 22:12:08 +0100

    try hard

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Defines amenities
-"""
+"""Contains the Amenity model"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Defines amenities that user can choose from to offer at its place"""
+    """Implements the Amenity model"""
     name = ""
