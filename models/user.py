586e7cbd17637ca127ef7f05ca7277a27c0a3eae
Olalekan Shogbaike <111006863+lekseel@users.noreply.github.com>
2023-02-12 22:20:54 +0100

    good mind

--- models/user.py
+++ models/user.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-User creation class
-"""
+"""Implements the user's model"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Defines attributes for user creation"""
+    """
+    Inherits from the BaseModel class and add user's functionalities
+    Args:
+        email (str): the email of the user
+        password (str): the password of the user
+        first_name (str): the first name of the user
+        last_name (str): the last name of the user
+    """
     email = ""
     password = ""
     first_name = ""
